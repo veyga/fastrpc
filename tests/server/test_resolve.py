a9c9ee81b82392ad632b19a6daae82c7a340b70b
Andrew Stefanich <andrew.stefanich@buoyhealth.com>
2024-09-21 20:22:28 -0400

    resolving path; just no fixture

--- tests/server/test_resolve.py
+++ tests/server/test_resolve.py
@@ -11,22 +11,22 @@ from types import SimpleNamespace as ___
 Case = lambda s: P.case(name=s, fix=s)
 
 
-@pytest.fixture
-def add_path():
-    def inner(dir, fix):
-        # original_sys_path = sys.path.copy()
-        print(f"INSERTING PATH {dir}")
-        path = Path(__file__).parent / "fixtures" / dir
-        sys.path.insert(0, str(path / fix))
-        yield
-        sys.path.pop(0)
+# @pytest.fixture
+# def add_path():
+#     def inner(dir, fix):
+#         path = Path(__file__).parent / "fixtures" / dir
+#         sys.path.insert(0, str(path / fix))
+#         yield 4
+#         sys.path.pop(0)
 
-    return inner
+#     return inner
 
 
 @P.autodetect_parameters()
 @Case("_1")
+@Case("_2")
 def test_ok(fix):
+    # x = add_path("ok", "_1")
     sys.path.insert(0, str(Path(__file__).parent / "fixtures" / "ok"))
     module = import_module(fix)
     print(module)
